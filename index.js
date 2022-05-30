1878174d2f7050e6ce6c0bf921b24b72eefdc95b
KukuJerung <khairulfilhan94@gmail.com>
2022-05-30 16:05:39 +0800

    add translator

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import {Alert} from 'react-native';
-import {setJSExceptionHandler, setNativeExceptionHandler} from './src/error-handler';
-import {fetchOriginalErrorLine, formatString, sendLog} from './src/send-error';
+import {defaultTitle} from './src/config';
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler,
+} from './src/error-handler';
+import {
+  fetchOriginalErrorLine,
+  formatString,
+  sendLog,
+} from './src/send-error';
 import {clear} from './src/utils/local-storage';
 import {checkIfItemExist} from './src/utils/shared';
 
@@ -10,41 +18,49 @@ let errMsg;
 let custInfo;
 let dvcInfo;
 
-
 const checkLocalData = async () => {
-  let data =  await checkIfItemExist('@error_logs');
+  let data = await checkIfItemExist('@error_logs');
   if (data) {
     await sendLog(apiUrl, JSON.parse(data), custInfo, dvcInfo);
     clear();
-
   }
 };
 
 const errorHandler = async (e, isFatal) => {
   let errString = JSON.stringify(e, Object.getOwnPropertyNames(e));
   const formattedString = formatString(errString);
-  const line =  formattedString[0];
-  const column =  formattedString[1];
-  let errorInfo =  await fetchOriginalErrorLine(line, column);
+  const line = formattedString[0];
+  const column = formattedString[1];
+  let errorInfo = await fetchOriginalErrorLine(line, column);
   // alert message
   if (isFatal) {
-    Alert.alert(errTitle, errMsg ? errMsg :  JSON.stringify(errorInfo), [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
-    sendLog(apiUrl, errString,  custInfo, dvcInfo);
+    Alert.alert(
+      errTitle ? errTitle : defaultTitle,
+      errMsg ? errMsg : JSON.stringify(errorInfo),
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ]
+    );
+    sendLog(apiUrl, errString, custInfo, dvcInfo);
   } else {
     console.log(e); // So that we can see it in the ADB logs in case of Android if needed
   }
 };
 
 export default {
-  init ({apiLogUrl = '', errorTitle = '', errorMessage = '', customerInfo  = '', deviceInfo = {}}) {
-    apiUrl = apiLogUrl;   
+  init ({
+    apiLogUrl = '',
+    errorTitle = '',
+    errorMessage = '',
+    customerInfo = '',
+    deviceInfo = {},
+  }) {
+    apiUrl = apiLogUrl;
     errMsg = errorMessage;
     errTitle = errorTitle;
     custInfo = customerInfo;
---
+++ src/utils/translator.js
@@ -0,0 +1,24 @@
+
+
+const filePath = 'index.android.bundle.map';
+
+var fs = require('fs');
+var sourceMapLib = require('source-map');
+var sourceMap = fs.readFileSync(filePath).toString();
+
+export const translate  = async () => {
+  const line = event.queryStringParameters.line;
+  const column = event.queryStringParameters.column;
+  return sourceMapLib.SourceMapConsumer.with(sourceMap, null, (consumer) => {
+    const result = consumer.originalPositionFor({
+      line: parseInt(line),
+      column: parseInt(column),
+    });
+    console.log(JSON.stringify(result, null, 2));
+    return {
+      statusCode: 200,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(result, null, 2),
+    };
+  });
+};
